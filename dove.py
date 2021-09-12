611cc26a9f85294e0c9d01237ae702dabe413c15
headset365 <jacob.eichner1999@gmail.com>
2021-09-12 22:10:46 +0930

    Updated try except for unresolvable url handeling

--- dove.py
+++ dove.py
@@ -43,17 +43,18 @@ def main(argv):
         if not line:
             break
         print("trying: " + line)
-        URL = "https://" + line
-        options = webdriver.ChromeOptions()
-        options.headless = True
-        driver = webdriver.Chrome(options=options)
-        driver.get(URL)
-    
-        S = lambda X: driver.execute_script('return document.body.parentNode.scroll'+X)
-        driver.set_window_size(S('Width'),S('Height')) # May need manual adjustment                                                                                                                
-        driver.find_element_by_tag_name('body').screenshot(line + '.png')
-
-        driver.quit()
+        try:
+            URL = "https://" + line
+            options = webdriver.ChromeOptions()
+            options.headless = True
+            driver = webdriver.Chrome(options=options)
+            driver.get(URL)
+            S = lambda X: driver.execute_script('return document.body.parentNode.scroll'+X)
+            driver.set_window_size(S('Width'),S('Height')) # May need manual adjustment                                                                                                                
+            driver.find_element_by_tag_name('body').screenshot(line + '.png')
+            driver.quit()
+        except:
+            print("failed " + line)
     file1.close()
 
 if __name__ == "__main__":
